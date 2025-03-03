949a409968c6d962de8f4d4b6afd3788f439c89c
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-03-03 14:46:23 +0200

    Add the IService for PositionResponsibilities

---
+++ Application/Interfaces/IPositionResponsibilityService.cs
@@ -0,0 +1,24 @@
+﻿using Application.DTOs;
+using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface IPositionResponsibilityService
+    {
+        Task AddPositionResponsibilitynAsync(PositionResponsibilityUpdateDto positionResponsibilityUpdateDto);
+
+        Task<IEnumerable<PositionResponsibilityEntity>> GetAllAsync();
+
+        Task<PositionResponsibilityEntity?> GetPositionResponsibilityByResponsibilityIdAsync(int id);
+
+        Task UpdatePositionResponsibilityAsync(PositionResponsibilityUpdateDto positionResponsibilityUpdateDto);
+
+        Task DeletePositionResponsibilityAsync(int id);
+
+        // Custom methods
+
+        Task DeleteResponsibilityByResponsibilityIdAndPositionIdAsync(int responsibilityId, int positionId);
+
+        Task<List<PositionResponsibilityDto>> GetPositionResponsibilitiesByPositionIdAsync(int positionId);
+    }
+}
