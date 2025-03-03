eab5e29295963adb8d0b275a7659dc898228d2db
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-03-03 14:18:29 +0200

    Modify the entities related to Projects to have data annotations

--- Domain/Entities/ProjectEntity.cs
+++ Domain/Entities/ProjectEntity.cs
@@ -1,22 +1,42 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entities
 {
+    [Table("Project")]
     public class ProjectEntity
     {
+        [Key]
+        [Column(TypeName = "int")]
         public int Id { get; set; }
 
         [Required]
-        public string Name { get; set; }
+        [Column(TypeName = "varchar(50)")]
+        public string ProjectName { get; set; } = string.Empty;
+
+        [Column(TypeName = "varchar(200)")]
+        public string Description { get; set; } = string.Empty;
 
         [Required]
-        public string StartDate { get; set; }
+        [Column(TypeName = "varchar(10)")]
+        public string StartDate { get; set; } = string.Empty;
 
-        public string EndDate { get; set; } = "Present";
-        public string Client { get; set; } = string.Empty;
+        [Column(TypeName = "varchar(10)")]
+        public string EndDate { get; set; } = string.Empty;
+
+        [Column(TypeName = "varchar(50)")]
         public string GithubUrl { get; set; } = string.Empty;
 
         [Required]
+        [Column(TypeName = "int")]
+        public int ProjectTypeId { get; set; }
+
+        [Required]
+        [Column(TypeName = "int")]
         public int UserId { get; set; }
+
+        public required ProjectTypeEntity ProjectType { get; set; } = null!;
+
+        public required UserEntity User { get; set; } = null!;
     }
 }
--- Domain/Entities/ProjectTechnologyEntity.cs
+++ Domain/Entities/ProjectTechnologyEntity.cs
@@ -1,16 +1,26 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entities
 {
+    [Table("ProjectTechnology")]
     public class ProjectTechnologyEntity
     {
+        [Key]
+        [Column(TypeName = "int")]
         public int Id { get; set; }
 
         [Required]
-        public required int ProjectId { get; set; }
+        [Column(TypeName = "int")]
+        public int ProjectId { get; set; }
 
         [Required]
-        public required int TechnologyId { get; set; }
+        [Column(TypeName = "int")]
+        public int TechnologyId { get; set; }
+
+        public ProjectEntity Project { get; set; } = null!;
+
+        public TechnologyEntity Technology { get; set; } = null!;
 
     }
 }
