bf208c51d236a2816719746a4c2559095339712f
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-03-03 14:45:54 +0200

    Add the DTO used to create and update the responsibilities of a position

---
+++ Application/DTOs/PositionResponsibilityUpdateDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.DTOs
+{
+    public class PositionResponsibilityUpdateDto
+    {
+
+        public int Id { get; set; }
+
+        public int? PositionId { get; set; }
+
+        public string? Responsibility { get; set; }
+    }
+}
