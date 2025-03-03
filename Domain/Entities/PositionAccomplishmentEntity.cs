2913a8815f865cc66dc5342480de118895d97f5d
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-03-03 14:23:23 +0200

    Add entity for project accomplishment

---
+++ Domain/Entities/PositionAccomplishmentEntity.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
+{
+    [Table("PositionAccomplishment")]
+    public class PositionAccomplishmentEntity
+    {
+        [Key]
+        [Column(TypeName = "int")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "int")]
+        public int PositionId { get; set; }
+
+        [Column(TypeName = "varchar(255)")]
+        public string Accomplishment { get; set; } = string.Empty;
+
+        public PositionEntity Position { get; set; } = null!;
+    }
+}
