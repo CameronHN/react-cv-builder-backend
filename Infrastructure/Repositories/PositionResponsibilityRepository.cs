f51e70af2c513e97e9de67fcb0a689940893eefc
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-03-03 14:40:30 +0200

    Add the implementation for the PositionResponsibility custom method

---
+++ Infrastructure/Repositories/PositionResponsibilityRepository.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class PositionResponsibilityRepository : Repository<PositionResponsibilityEntity>, IPositionResponsibilityRepository
+    {
+
+        private readonly DataContext _dataContext;
+
+        public PositionResponsibilityRepository(DataContext dataContext) : base(dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task<List<PositionResponsibilityEntity>> GetPositionResponsibilitiesByPositionIdAsync(int positionId)
+        {
+
+            try
+            {
+                return await _dataContext.PositionResponsibilities
+                    .Where(p => p.PositionId == positionId)
+                    .ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
