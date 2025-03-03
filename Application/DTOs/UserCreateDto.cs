fbeeb21b1b37e428d40a146bbc36fc06c6794d81
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-03-03 14:42:03 +0200

    Add the DTO for creating a User record

---
+++ Application/DTOs/UserCreateDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs
+{
+    public class UserCreateDto
+    {
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+
+        public string? MiddleName { get; set; } = string.Empty;
+
+        [Required]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        public string? PhoneNumber { get; set; } = string.Empty;
+    }
+}
