a64065668d0dd4395521c63f164c2b3c9627ea3d
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-03-03 14:22:25 +0200

    Add entity for project type

---
+++ Domain/Entities/ProjectTypeEntity.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
+{
+    [Table("ProjectType")]
+    public class ProjectTypeEntity
+    {
+        [Column(TypeName = "int")]
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        public string ProjectTypeName { get; set; } = string.Empty;
+
+        public ProjectEntity ProjectEntity { get; set; } = null!;
+    }
+}
