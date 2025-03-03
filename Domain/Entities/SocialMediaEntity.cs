69fb0afce4139b7d9e5f43359202fe1423c24a7b
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-03-03 14:21:15 +0200

    Modify social media entity to have data annotations

--- Domain/Entities/SocialMediaEntity.cs
+++ Domain/Entities/SocialMediaEntity.cs
@@ -1,18 +1,27 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entities
 {
+    [Table("SocialMedia")]
     public class SocialMediaEntity
     {
+        [Key]
+        [Column(TypeName = "int")]
         public int Id { get; set; }
 
         [Required]
-        public string Name { get; set; }
+        [Column(TypeName = "varchar(50)")]
+        public string SocialMediaName { get; set; } = string.Empty;
 
         [Required]
-        public string Url { get; set; }
+        [Column(TypeName = "varchar(50)")]
+        public string SocialMediaUrl { get; set; } = string.Empty;
 
         [Required]
+        [Column(TypeName = "int")]
         public int UserId { get; set; }
+
+        public required UserEntity User { get; set; } = null!;
     }
 }
