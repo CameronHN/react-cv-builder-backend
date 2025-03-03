ff9620051125fa34cf5690253545dc33ab95c4a2
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-03-03 14:16:46 +0200

    Modify the entities related to Positions to have data annotations

--- Domain/Entities/PositionEntity.cs
+++ Domain/Entities/PositionEntity.cs
@@ -1,25 +1,36 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entities
 {
+    [Table("Position")]
     public class PositionEntity
     {
+        [Key]
+        [Column(TypeName = "int")]
         public int Id { get; set; }
 
         [Required]
-        public string Role {  get; set; } = string.Empty;
+        [Column(TypeName = "varchar(50)")]
+        public string Role { get; set; } = string.Empty;
 
         [Required]
+        [Column(TypeName = "varchar(10)")]
         public string StartDate { get; set; } = string.Empty;
 
-        public string EndDate { get; set; } = "Present";
+        [Required]
+        [Column(TypeName = "varchar(10)")]
+        public string EndDate { get; set; } = string.Empty;
 
         // Foreign key
         [Required]
+        [Column(TypeName = "int")]
         public int UserId { get; set; }
 
         // Navigation property
         public UserEntity User { get; set; } = null!;
 
+        // Navigation property
+        public ICollection<PositionResponsibilityEntity>? Responsibilities { get; set; }
     }
 }
--- Domain/Entities/PositionResponsibilityEntity.cs
+++ Domain/Entities/PositionResponsibilityEntity.cs
@@ -1,15 +1,24 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entities
 {
+    [Table("PositionResponsibility")]
     public class PositionResponsibilityEntity
     {
+        [Key]
+        [Column(TypeName = "int")]
         public int Id { get; set; }
 
         [Required]
+        [Column(TypeName = "int")]
         public int PositionId { get; set; }
 
+        [Required]
+        [Column(TypeName = "varchar(255)")]
         public string Responsibility { get; set; } = string.Empty;
+
+        public PositionEntity Position { get; set; } = null!;
 
     }
 }
