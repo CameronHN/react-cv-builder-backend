4d2770c7b42e53e87bbb45d809677b0a3fdaa789
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-03-03 14:23:32 +0200

    Add entity for project outcome

---
+++ Domain/Entities/ProjectOutcomeEntity.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
+{
+    [Table("ProjectOutcome")]
+    public class ProjectOutcomeEntity
+    {
+        [Key]
+        [Column(TypeName = "int")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "int")]
+        public int ProjectId { get; set; }
+
+        [Column(TypeName = "varchar(150)")]
+        public string Outcome { get; set; } = string.Empty;
+
+        public ProjectEntity Project { get; set; } = null!;
+    }
+}
