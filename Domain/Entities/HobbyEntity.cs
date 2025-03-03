b819629c3dbcf1df8ee112f08d15a84f18d9a98b
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-03-03 14:23:00 +0200

    Add entities for hobby and hobby type

---
+++ Domain/Entities/HobbyEntity.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
+{
+    [Table("Hobby")]
+    public class HobbyEntity
+    {
+        [Key]
+        [Column(TypeName = "int")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        public string HobbyName { get; set; } = string.Empty;
+
+        [Required]
+        [Column(TypeName = "int")]
+        public int HobbyTypeId { get; set; }
+
+        [Required]
+        [Column(TypeName = "int")]
+        public int UserId { get; set; }
+
+        public HobbyTypeEntity HobbyType { get; set; } = null!;
+
+        public UserEntity User { get; set; } = null!;
+    }
+}
---
+++ Domain/Entities/HobbyTypeEntity.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
+{
+    [Table("HobbyType")]
+    public class HobbyTypeEntity
+    {
+        [Key]
+        [Column(TypeName = "int")]
+        public int Id { get; set; }
+
+        [Column(TypeName = "varchar(20)")]
+        [Required]
+        public string HobbyTypeName { get; set; } = string.Empty;
+
+        public ICollection<HobbyEntity> Hobbies { get; set; } = new List<HobbyEntity>();
+    }
+}
