8f34d91e60a42ebd05f50efd53a8ef4e15c696a6
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-03-03 14:24:30 +0200

    Add custom interface repository method for getting position responsibilities

---
+++ Domain/Interfaces/IPositionResponsibilityRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IPositionResponsibilityRepository : IRepository<PositionResponsibilityEntity>
+    {
+        Task<List<PositionResponsibilityEntity>> GetPositionResponsibilitiesByPositionIdAsync(int positionId);
+    }
+}
