f2401810c28f29ce68a581075bfd3bfc07d51491
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-03-06 20:11:57 +0200

    Add additional testing migrations

---
+++ Infrastructure/Migrations/20250304214546_FixPrimaryKey.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixPrimaryKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Project_ProjectTypeId",
+                table: "Project");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Project_ProjectTypeId",
+                table: "Project",
+                column: "ProjectTypeId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Project_ProjectTypeId",
+                table: "Project");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Project_ProjectTypeId",
+                table: "Project",
+                column: "ProjectTypeId");
+        }
+    }
+}
