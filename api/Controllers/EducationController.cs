d954e3c85b635678c55ea289ccb626fa557fbd4f
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-03-10 16:22:53 +0200

    Complete controller with custom method to get all of the user's education by their Id

---
+++ api/Controllers/EducationController.cs
@@ -0,0 +1,29 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EducationController : ControllerBase
+    {
+        private readonly IEducationService _educationService;
+
+        public EducationController(IEducationService educationService)
+        {
+            _educationService = educationService;
+        }
+
+        [HttpGet("user/{userId}")]
+        public async Task<ActionResult<List<EducationEntity>>> GetEducationByUserId(int userId)
+        {
+            var educationList = await _educationService.GetEducationByUserIdAsync(userId);
+            if (educationList == null || educationList.Count == 0)
+            {
+                return NotFound($"No education records found for user with ID: {userId}.");
+            }
+            return Ok(educationList);
+        }
+    }
+}
