811320c79ba14fb3068c0cef539de67ae8f47944
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-03-10 16:26:40 +0200

    Add interfaces for custom methods (User Skills, Skill Types, Education)

---
+++ Application/Interfaces/IUserSkillService.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface IUserSkillService
+    {
+        Task<List<UserSkillEntity>> GetUserSkillsByUserIdAsync(int userId);
+
+        Task<string> GetSkillNameBySkillIdAsync(int skillId);
+
+        Task<List<UserSkillEntity>> GetUserSkillsWithSkillTypeByUserIdAsync(int userId);
+
+    }
+}
---
+++ Domain/Interfaces/IUserSkillRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IUserSkillRepository
+    {
+        Task<List<UserSkillEntity>> GetUserSkillsByUserIdAsync(int userId);
+
+        Task<string> GetSkillNameBySkillIdAsync(int skillId);
+
+        Task<List<UserSkillEntity>> GetUserSkillsWithSkillTypeByUserIdAsync(int userId);
+
+    }
+}
