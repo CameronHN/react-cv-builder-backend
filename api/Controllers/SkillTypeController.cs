b9829c2ffa9181a922f16399b4f73be555afdf25
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-03-10 16:23:54 +0200

    Complete controller for Skill Types with custom method

---
+++ api/Controllers/SkillTypeController.cs
@@ -0,0 +1,29 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SkillTypeController : ControllerBase
+    {
+        private readonly ISkillTypeService _skillTypeService;
+
+        public SkillTypeController(ISkillTypeService skillTypeService)
+        {
+            _skillTypeService = skillTypeService;
+        }
+
+        [HttpGet("{skillTypeId}/skills")]
+        public async Task<ActionResult<SkillTypeEntity>> GetSkillTypeWithSkills(int skillTypeId)
+        {
+            var skillType = await _skillTypeService.GetSkillTypeWithSkillsAsync(skillTypeId);
+            if (skillType == null)
+            {
+                return NotFound();
+            }
+            return Ok(skillType);
+        }
+    }
+}
