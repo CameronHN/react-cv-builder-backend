46e8c949e35bdb101539f57cc94bb4c60eddfa38
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-03-18 14:13:46 +0200

    Use DTO for the GetAll and GetById

--- Application/Interfaces/IUserService.cs
+++ Application/Interfaces/IUserService.cs
@@ -1,20 +1,15 @@
 ﻿using Application.DTOs;
 using Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Interfaces
 {
     public interface IUserService
-    {       
+    {
         Task AddUserAsync(UserEntity user);
 
-        Task<IEnumerable<UserEntity>> GetAllAsync();
+        Task<IEnumerable<UserDto>> GetAllAsync();
 
-        Task<UserEntity?> GetUserByIdAsync(int id);
+        Task<UserDto> GetUserByIdAsync(int id);
 
         Task UpdateUserAsync(UserUpdateDto userDto);
 
