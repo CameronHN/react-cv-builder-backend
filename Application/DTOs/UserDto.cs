6736bd877aad4922cd33e4bc22a2db2dbbcaba5f
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-03-18 14:13:21 +0200

    Create DTO to filter the results of getting a User/Users

---
+++ Application/DTOs/UserDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Application.DTOs
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string? MiddleName { get; set; }
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string? PhoneNumber { get; set; }
+    }
+}
